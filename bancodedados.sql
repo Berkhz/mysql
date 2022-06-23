f0fa171d0aab75c7f13b057cbc13060c747c9f81
Berkhz <kauan.hb2004@gmail.com>
2022-06-23 16:47:02 -0300

    Initial commit

---
+++ bancodedados.sql
@@ -0,0 +1,10 @@
+create database pessoa;
+
+create table pessoas (
+    nome varchar(30),
+    idade varchar(3),
+    sexo varchar(1),
+    peso float,
+    altura float,
+    nacionalidade varchar(20) 
+);

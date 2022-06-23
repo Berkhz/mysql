60199f5ed35330c1af52a0ee7cebf3cdee79c62f
Berkhz <kauan.hb2004@gmail.com>
2022-06-23 17:40:20 -0300

    update sql

--- bancodedados.sql
+++ bancodedados.sql
@@ -1,6 +1,9 @@
 create database cadastro; -- Cria um banco de dados;
 drop database cadastro -- Exclui um banco de dados;
 
+default character set utf8 
+default collate utf8_general_ci;
+
 create table pessoas (
     nome varchar(30),
     idade varchar(3),

828de28588e3f325bba6a14bb24d597f97172ec8
Berkhz <kauan.hb2004@gmail.com>
2022-06-24 16:55:48 -0300

    update mysql

--- bancodedados.sql
+++ bancodedados.sql
@@ -19,10 +19,12 @@ DEFAULT CHARACTER SET utf8 -- Constraint
 DEFAULT COLLATE utf8_general_ci -- Collation 
 
 CREATE TABLE pessoas (
+    id INT NOT NULL AUTO_INCREMENT,
     nome VARCHAR(30) NOT NULL,
     nascimento DATE,
     sexo enum('M','F'),
     peso DECIMAL(5,2),
     altura DECIMAL(3,2),
-    nacionalidade VARCHAR(20) DEFAULT 'Brasil'
+    nacionalidade VARCHAR(20) DEFAULT 'Brasil',
+    PRIMARY KEY (id)
 )DEFAULT CHARSET  = utf8;

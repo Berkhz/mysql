b71804e5c628d45e43711ccfaef99484f120e48d
Berkhz <kauan.hb2004@gmail.com>
2022-06-24 15:03:07 -0300

    Update mysql

--- bancodedados.sql
+++ bancodedados.sql
@@ -1,10 +1,8 @@
-create database cadastro; -- Cria um banco de dados;
-drop database cadastro -- Exclui um banco de dados;
+-- Criando o primeiro banco de dados
+create database cadastro; -- Cria um banco de dados
+drop database cadastro -- Exclui um banco de dados
 
-default character set utf8 
-default collate utf8_general_ci;
-
-create table pessoas (
+create table pessoas ( -- Cria uma tabela no banco de dados
     nome varchar(30),
     idade varchar(3),
     sexo varchar(1),
@@ -12,3 +10,10 @@ create table pessoas (
     altura float,
     nacionalidade varchar(20) 
 );
+
+describe pessoas;
+
+-- Melhorando a Estrutura do Banco de Dados
+create database cadastro
+default character set utf8 -- Constraint
+default collate utf8_general_ci -- Collation 

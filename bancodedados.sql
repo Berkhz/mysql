f4025462e3960ea33046fdedfa40c921bf000f66
Berkhz <kauan.hb2004@gmail.com>
2022-06-24 16:43:21 -0300

    Update mysql

--- bancodedados.sql
+++ bancodedados.sql
@@ -1,19 +1,28 @@
 -- Criando o primeiro banco de dados
-create database cadastro; -- Cria um banco de dados
-drop database cadastro -- Exclui um banco de dados
+CREATE DATABASE cadastro; -- Cria um banco de dados
+DROP DATABASE cadastro -- Exclui um banco de dados
 
-create table pessoas ( -- Cria uma tabela no banco de dados
-    nome varchar(30),
-    idade varchar(3),
-    sexo varchar(1),
-    peso float,
-    altura float,
-    nacionalidade varchar(20) 
+CREATE TABLE pessoas ( -- Cria uma tabela no banco de dados
+    nome VARCHAR(30), -- VARCHAR = texto de comprimento variável (CHAR = texto de comprimento fixo)
+    idade VARCHAR(3),
+    sexo VARCHAR(1),
+    peso FLOAT,
+    altura FLOAT,
+    nacionalidade VARCHAR(20) 
 );
 
-describe pessoas;
+describe TABLE; 
 
 -- Melhorando a Estrutura do Banco de Dados
-create database cadastro
-default character set utf8 -- Constraint
-default collate utf8_general_ci -- Collation 
+CREATE DATABASE cadastro
+DEFAULT CHARACTER SET utf8 -- Constraint
+DEFAULT COLLATE utf8_general_ci -- Collation 
+
+CREATE TABLE pessoas (
+    nome VARCHAR(30) NOT NULL,
+    nascimento DATE,
+    sexo enum('M','F'),
+    peso DECIMAL(5,2),
+    altura DECIMAL(3,2),
+    nacionalidade VARCHAR(20) DEFAULT 'Brasil'
+)DEFAULT CHARSET  = utf8;
